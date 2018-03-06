3d334ce85fc74d9643e05d9f148688a5a270021c
Denes Bodo <bod.denes@gmail.com>
2018-03-06 21:16:50 +0100

    Fix for  * MySQL limit  * non-quoted expression

--- backend/src/main/java/hu/denes/bme/dipterv/data/sql/MySqlQuery.java
+++ backend/src/main/java/hu/denes/bme/dipterv/data/sql/MySqlQuery.java
@@ -3,6 +3,9 @@ package hu.denes.bme.dipterv.data.sql;
 public class MySqlQuery extends Query {
     @Override
     public String toString() {
+        if (this.limit == null) {
+            return super.toString();
+        }
         return super.toString() + " LIMIT " + this.limit;
     }
 }
